27006520790fefbd15d7b433ab033cf0e98b3913
Kudan1l <alpsosial@gmail.com>
2024-12-15 21:30:21 +0700

    Menambahkan project Java

---
+++ src/main/java/com/SoLAW/SoLawApplication.java
@@ -0,0 +1,13 @@
+package com.SoLAW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoLawApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoLawApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/SoLAW/SoLawApplicationTests.java
@@ -0,0 +1,13 @@
+package com.SoLAW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoLawApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
