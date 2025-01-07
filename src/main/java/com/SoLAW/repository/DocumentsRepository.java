d7baa61600471330ca56a52582df795eb1fe01ca
illise <nurrohimahmadrohman@gmail.com>
2025-01-07 13:09:19 +0700

    buatSurat

---
+++ src/main/java/com/SoLAW/repository/DocumentsRepository.java
@@ -0,0 +1,16 @@
+package com.SoLAW.repository;
+
+import com.SoLAW.model.Articles;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ArticlesRepository extends JpaRepository<Articles, Integer> {
+
+    List<Articles> findAll(Sort sort);
+
+    List<Articles> findTop3ByOrderByCreatedAtDesc();
+}
